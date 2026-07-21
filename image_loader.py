930d8386674319cc7586cab681b673d4f43ba027
Daniel Martin <danielamartin314@gmail.com>
2026-07-21 15:34:50 -0700

    test

--- image_loader.py
+++ image_loader.py
@@ -1,5 +1,12 @@
 from workbook_parser import WorkbookParser
 
+######################################################################
+# ImageLoader is currently a neat wrapper for WorkbookParser. In the
+# future, there will be more functionality from this class, but there
+# is only trivial calling of the WorkbookParser getImage function as
+# of now.
+######################################################################
+
 class ImageLoader:
 
     def __init__(self, path_to_workbook: str):
